7a4cd662761d22ec7fd64969d50187614a1ed3f0
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-11 17:48:10 -0400

    Added max lengths to the url and name properties.

--- RandomFiles/Models/RandomVideo.cs
+++ RandomFiles/Models/RandomVideo.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RandomFiles.Models
 {
     class RandomVideo
     {
         public int Id { get; set; }
+        [MaxLength(255)]
         public string FullUrl { get; set; }
+        [MaxLength(30)]
         public string VideoName { get; set; }
         public int TimesPlayed { get; set; }
         public DateTime DateCreated { get; set; }
