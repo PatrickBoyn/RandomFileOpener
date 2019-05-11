8c07f6eb1b9ee3dc9d8ac32557629406f41a95b5
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-10 20:01:29 -0400

    Added the RandomVideo properties.

--- RandomFiles/Models/RandomVideo.cs
+++ RandomFiles/Models/RandomVideo.cs
@@ -1,6 +1,13 @@
-﻿namespace RandomFiles.Models
+﻿using System;
+
+namespace RandomFiles.Models
 {
     class RandomVideo
     {
+        public int Id { get; set; }
+        public string FullUrl { get; set; }
+        public string VideoName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastAccessed { get; set; }
     }
 }
