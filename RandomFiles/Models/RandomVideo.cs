d486a32ba123e4099705badfdda4b47159f018c8
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-11 02:15:04 -0400

    Added a TimesPlayed column to RandomVideos.

--- RandomFiles/Models/RandomVideo.cs
+++ RandomFiles/Models/RandomVideo.cs
@@ -7,6 +7,7 @@ namespace RandomFiles.Models
         public int Id { get; set; }
         public string FullUrl { get; set; }
         public string VideoName { get; set; }
+        public int TimesPlayed { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateLastAccessed { get; set; }
     }
