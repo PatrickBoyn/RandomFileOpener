ba9ecbcb939f6bee15d74e6acd353851778a315d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-11 16:52:22 -0400

    Tested to see if the log movies method worked.

--- RandomFiles/Program.cs
+++ RandomFiles/Program.cs
@@ -7,6 +7,9 @@ namespace RandomFiles
     {
         static void Main(string[] args)
         {
+            var getMovies = new GetRandomMovies();
+
+            getMovies.LogMovies();
 
             Console.ReadKey();
         }
