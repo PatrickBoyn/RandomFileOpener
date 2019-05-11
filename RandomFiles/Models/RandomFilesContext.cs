7bf5d67651f9051d630706f1360136b273f2fedf
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-11 00:30:04 -0400

    Added a db set for the random games table.

--- RandomFiles/Models/RandomFilesContext.cs
+++ RandomFiles/Models/RandomFilesContext.cs
@@ -5,6 +5,7 @@ namespace RandomFiles.Models
     class RandomFilesContext : DbContext
     {
         public DbSet<RandomVideo> RandomVideos { get; set; }
+        public DbSet<RandomGame> RandomGames { get; set; }
         public RandomFilesContext() : base("name=DefaultConnection")
         {
             
