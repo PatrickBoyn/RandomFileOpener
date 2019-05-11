e8972b890ac4804e6184e926782f901c846bca5c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-11 16:45:00 -0400

    Added a file launcher method.

--- RandomFiles/API/LaunchFiles.cs
+++ RandomFiles/API/LaunchFiles.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace RandomFiles.API
 {
     class LaunchFiles
     {
+        public void PlayMovies()
+        {
+            var getMovies = new GetRandomMovies();
+            Process.Start(getMovies.GetRandomMovie());
+        }
     }
 }
