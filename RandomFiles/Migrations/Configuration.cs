835dd28022c085905a23030bc7723b2641db3520
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-10 21:38:55 -0400

    Enabled migrations.

---
+++ RandomFiles/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace RandomFiles.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RandomFiles.Models.RandomFilesContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(RandomFiles.Models.RandomFilesContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
