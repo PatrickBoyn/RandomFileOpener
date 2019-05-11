fe73065047de893d30d72933f92c947d663f4e52
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-11 17:47:50 -0400

    Changed the name of a property, and added max length to the name, and url properties.

--- RandomFiles/Models/RandomGame.cs
+++ RandomFiles/Models/RandomGame.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RandomFiles.Models
 {
     class RandomGame
     {
         public int Id { get; set; }
+        [MaxLength(255)]
         public string FullUrl { get; set; }
-        public string VideoName { get; set; }
+        [MaxLength(30)]
+        public string GameName { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateLastAccessed { get; set; }
     }
