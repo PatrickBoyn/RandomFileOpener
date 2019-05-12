795152e174dcd52d5ab7fab11efdfa46fede8d13
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-12 15:06:30 -0400

    Added the logging methods here.

--- RandomFiles/API/LogFiles.cs
+++ RandomFiles/API/LogFiles.cs
@@ -1,6 +1,25 @@
-﻿namespace RandomFiles.API
+﻿using System;
+
+namespace RandomFiles.API
 {
     public static class LogFiles
     {
+        //For diagnostic purposes.
+        public static void LogMovies()
+        {
+            var getMovies = new GetRandomFiles();
+            string[] mp4s = getMovies.GetFiles();
+
+            foreach (string mp4 in mp4s)
+            {
+                Console.WriteLine(mp4);
+            }
+        }
+
+        //For diagnostic purposes.
+        public static void LogRandomMovie(string randomMovie)
+        {
+            Console.WriteLine(randomMovie);
+        }
     }
 }
