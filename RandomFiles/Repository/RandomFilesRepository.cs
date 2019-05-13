0cdbaf43532f6fa635846223f5d7be64cdafe026
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-13 17:47:21 -0400

    Implimented the missing members.

--- RandomFiles/Repository/RandomFilesRepository.cs
+++ RandomFiles/Repository/RandomFilesRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace RandomFiles.Repository
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace RandomFiles.Repository
 {
     class RandomFilesRepository<TEntity> : IRepository<TEntity> where TEntity : class
     {
+        public TEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(TEntity tEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
