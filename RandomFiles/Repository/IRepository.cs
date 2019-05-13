e3622a8d91948f6707db5a6aadbe6c784ecc01b3
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-13 17:31:57 -0400

    Added the details of the interface.

--- RandomFiles/Repository/IRepository.cs
+++ RandomFiles/Repository/IRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace RandomFiles.Repository
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace RandomFiles.Repository
 {
     interface IRepository<TEntity> where TEntity : class
     {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        void Add(TEntity tEntity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
     }
 }
