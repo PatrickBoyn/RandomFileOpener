aeee01f31cc738092f0cc1df997c5de1879d905f
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-12 22:55:34 -0400

    Moved the random declaration, made a huge difference in the overall randomness.

--- RandomFiles/API/GetRandomFiles.cs
+++ RandomFiles/API/GetRandomFiles.cs
@@ -7,7 +7,7 @@ namespace RandomFiles.API
     {
         private readonly string _path;
         private readonly string _type;
-        
+        Random rand = new Random();
         public GetRandomFiles(string path= @"C:\Users\dakil\Desktop\Extras", string type="*.mp4")
         {
             _path = path;
@@ -16,7 +16,6 @@ namespace RandomFiles.API
 
         public string[] GetFiles()
         {
-            string test =Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             string path = _path;
             string[] filePaths = Directory.GetFiles(_path, _type);
             return filePaths;
@@ -25,7 +24,6 @@ namespace RandomFiles.API
         //Gets a random movie from the list created above.
         public string GetRandomMovie()
         {
-            var rand = new Random();
             string[] getMovieList = GetFiles();
 
             int length = getMovieList.Length;
