97b7a22252a7324bad43782c76ca7123a1dd2fc4
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-13 20:09:33 -0400

    Not sure what's different.

---
+++ RandomFiles/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RandomFiles.Repository
+{
+    class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+        }
+        public TEntity Get(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _context.Set<TEntity>().Where(predicate);
+        }
+
+        public void Add(TEntity tEntity)
+        {
+             _context.Set<TEntity>().Add(tEntity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().AddRange(entities);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
