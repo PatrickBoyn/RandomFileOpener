ae3a51b83772efc8ccbac8b90ef241e2c5fe5bea
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-20 21:25:42 -0400

    Added the basic IRepo interface.

--- RandomFiles/Repository/IRepository.cs
+++ RandomFiles/Repository/IRepository.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace RandomFiles.Repository
 {
     interface IRepository<TEntity> where TEntity : class
     {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        void Add(TEntity entity);
+        void AddAll(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveAll(IEnumerable<TEntity> entities);
     }
 }
